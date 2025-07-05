586229818b0c7db4cb19c3e096d53439c047e369
mohitsh <mohitsh114@gmail.com>
2025-07-05 13:06:41 +0530

    updated user schema with response and update classes

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel, EmailStr
 
 
@@ -7,3 +9,20 @@ class UserCreate(BaseModel):
     email: EmailStr  # validates email format
     company: str
     jobTitle: str
+
+
+class UserResponse(BaseModel):
+    uuid: str
+    name: str
+    surname: str
+    email: EmailStr  # validates email format
+    company: str
+    jobTitle: str
+
+
+class UserUpdate(BaseModel):
+    name: Optional[str] = None
+    surname: Optional[str] = None
+    email: Optional[EmailStr] = None
+    company: Optional[str] = None
+    jobTitle: Optional[str] = None
