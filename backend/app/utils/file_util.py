354ac97adda84789781e86193918b6d3633f9b2f
mohitsh <mohitsh114@gmail.com>
2025-07-05 14:47:57 +0530

    file util is ready

--- backend/app/utils/file_util.py
+++ backend/app/utils/file_util.py
@@ -1,14 +1,21 @@
+import json
+import os
+
+from ..config import DATA_FILE_PATH
+
+
 def read_data():
-    pass
+    if file_exists():
+        with open(DATA_FILE_PATH, "r") as f:
+            return json.load(f)
+    return None
 
 
-def write_data():
-    pass
-
-
-def create_file_if_not_exists():
-    pass
+def write_data(data):
+    with open(DATA_FILE_PATH, "w") as f:
+        json.dump(data, f, indent=2)
 
 
 def file_exists():
-    pass
+    return os.path.exists(DATA_FILE_PATH)
+
