d87f51b60ea9306451157a0f6609852bf8f7579e
mohitsh <mohitsh114@gmail.com>
2025-07-06 00:37:23 +0530

    adding routers for get/put/post

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -1,18 +1,26 @@
 from fastapi import APIRouter
 
+from ..repositories.user_repository import UserRepository
+from ..schemas.user import UserCreate, UserUpdate
+from ..services.user_service import UserService
+
 router = APIRouter()
 
-MOCK_USERS = [
-    {
-      "uuid": "123e4567-e89b-12d3-a456-426614174000",
-      "name": "John",
-      "surname": "Doe",
-      "email": "john.doe@example.com",
-      "company": "BlackRock",
-      "jobTitle": "Financial Analyst"
-    },
-  ]
+# service instances
+user_repository = UserRepository()
+user_service = UserService(user_repository)
+
 
 @router.get("/api/users")
 def get_users():
-    return MOCK_USERS
+    return {}
+
+
+@router.post("/api/users")
+def create_users(user_create: UserCreate):
+    return {}
+
+
+@router.put("/api/users/{uuid}")
+def update_user(uuid: str, user_update: UserUpdate):
+    return {}
