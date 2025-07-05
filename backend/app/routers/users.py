63f0ee1aedfc58e92b28cd8ce27b97204032348f
mohitsh <mohitsh114@gmail.com>
2025-07-06 01:12:14 +0530

    addig functioning POST api

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -13,16 +13,23 @@ user_service = UserService(user_repository)
 
 @router.get("/api/users")
 def get_users():
-    # try:
-    users = user_service.get_all_users()
-    return {"success": True, "users": users}
-    # except Exception as e:
-    #     raise HTTPException(status_code=500, detail="Internal Server Error")
+    try:
+        users = user_service.get_all_users()
+        return {"success": True, "users": users}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal Server Error")
 
 
 @router.post("/api/users")
 def create_users(user_create: UserCreate):
-    return {}
+    try:
+        user = user_service.create_user(user_create)
+        if user:
+            return {"success": True, "user": user}
+        else:
+            raise HTTPException(status_code=400, detail="Failed to crate user")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal Server Error")
 
 
 @router.put("/api/users/{uuid}")
