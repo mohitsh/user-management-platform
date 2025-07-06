1b2c6825b40858a3ffc89e62a09782b43b634d0d
mohitsh <mohitsh114@gmail.com>
2025-07-06 12:35:01 +0530

    allowing cors for local testing, (allow port 3000 of front end for backend)

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import users
 
 app = FastAPI()
 
+# cors middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # local frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
+
 
 app.include_router(users.router)
